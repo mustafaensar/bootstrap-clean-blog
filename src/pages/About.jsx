98588341fc0e511261277fdadd19faabfc26ff7e
mustafaensar <mustafaensar55@gmail.com>
2023-04-28 22:02:22 +0200

    pages are created

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+function About() {
+  return (
+    <>
+      <Header title="About Me" description="This is what I do." img="about"/>
+    </>
+  )
+}
+
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+function Contact() {
+  return (
+    <>
+      <Header title="Contact Me" description="Have questions? I have answers." img="contact"/>
+    </>
+  )
+}
+
+export default Contact;
